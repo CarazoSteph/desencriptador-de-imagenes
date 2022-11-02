ab1e44a589e6b56f510dd2d0129f74ff9e925b99
Luis Andrey <luisandreyzuniga@gmail.com>
2022-11-02 12:24:15 -0600

    Codigo para decodificar
    Todavia es necesario realizar pruebas

--- ProyectoQuartus/instructionDecoder.sv
+++ ProyectoQuartus/instructionDecoder.sv
@@ -1,31 +1,77 @@
-module instructionDecoder(input [31:0] instruction);
-
-	logic [1:0] opType; //tipo de operacion: memeoria, datos o branch
-	logic [3:0]flags; //banderas del condicional
+module instructionDecoder(instruction,op,funct,cond,Rn,Rm,Rd,imm);
+	input [31:0] instruction;
+	output logic [1:0] op;
+	output logic [5:0] funct;
+	output logic [3:0] cond;
+	output logic [3:0] Rn,Rm,Rd; //Rn=Primer operador, Rm=Segundo operador en caso de usarse,  Rd=registro destino
+	output logic [23:0] imm; //inmediato
+	//logic [1:0] indexMode;
+	logic I;//bandera de inmediato
+	always_comb begin
+	
+		case(op)
+		
+			0:	
+			begin
+				Rn=instruction[19:16];
+				Rd=instruction[15:12];
+				funct=instruction[25:20];
+				//indexMode=0;
+				if(I) begin
+					
+					imm=instruction[7:0];
+					Rm=0;
+				end
+				else begin
+					imm=0;
+					Rm=instruction[3:0];
+				end
+			end
+			
+			1: 
+			begin
+				Rn=instruction[19:16];
+				Rd=instruction[15:12];
+				funct=instruction[25:20];
+				//indexMode={instruction[24],instruction[21]};
+				if(I)begin
+					Rm=instruction[3:0];
+					imm=0;
+				
+				end
+				else begin
+					Rm=0;
+					imm=instruction[11:0];
+				
+				end
+			end
+			2:
+			begin
+				Rn=0;
+				Rd=0;
+				Rm=0;
+				funct=instruction[25:24];
+				imm=instruction[23:0];
+			end
+			default:
+			begin
+				Rm=0;
+				Rn=0;
+				Rd=0;
+				funct=0;
+				imm=0;
+			
+			end
+		
+		
+		endcase
 	
 	
-	//Para el caso de Data processing
-	logic ImmCondition; //Codicion para immediate
-	logic [3:0] OpCode; //operacion a solicitar
-	logic S;  //condition codes: verifica si tiene en cuenta las banderas para ejecutar
-	logic [3:0] Rn; //Primer registro operando
-	logic [3:0] Rd; //Registro Destino
-	logic [11:0] operand2; //segundo operando, podria tener dos casos un registro o un inmediato
-	logic [3:0] Rm; //segundo operando en caso de utilizarse
-	logic [1:0] shiftType; //tipo shift en caso de existir
-	logic [4:0] shiftAmount; //numero de shift guardado en un inmediato
-	logic [3:0] shiftRegister; //numero de registro en donde se almacena el shift
-	logic shiftMode;//determina si el shift que se lleva a cabo es con un inmediato o con el contenido de un registro
-	logic [3:0] rotate; //rotacion para el caso de segundo operando inmediato
-	logic [7:0] imm; //valor del inmediato
+	end
+	assign op=instruction[27:26];
+	assign cond=instruction[31:28];
+	assign I=instruction[25];
 	
-	//Para el caso de branch 
-	
-	logic L; //link bit
-	logic [23:0] offset;
-	
-	assign opType=instruction[27:26];
-	assign flags=instruction[31:28];
 	
 	
 
--- ProyectoQuartus/simulation/modelsim/Processor_run_msim_rtl_verilog.do
+++ ProyectoQuartus/simulation/modelsim/Processor_run_msim_rtl_verilog.do
@@ -6,9 +6,9 @@ vlib rtl_work
 vmap work rtl_work
 
 vlog -vlog01compat -work work +incdir+C:/TEC/Segundo\ Semestre\ 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus {C:/TEC/Segundo Semestre 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus/instructionMemory.v}
-vlog -sv -work work +incdir+C:/TEC/Segundo\ Semestre\ 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus {C:/TEC/Segundo Semestre 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus/instructionGetter.sv}
 vlog -sv -work work +incdir+C:/TEC/Segundo\ Semestre\ 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus {C:/TEC/Segundo Semestre 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus/instructionMemoryProgramCounter.sv}
 vlog -sv -work work +incdir+C:/TEC/Segundo\ Semestre\ 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus {C:/TEC/Segundo Semestre 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus/genericCounter.sv}
+vlog -sv -work work +incdir+C:/TEC/Segundo\ Semestre\ 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus {C:/TEC/Segundo Semestre 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus/instructionGetter.sv}
 
 vlog -sv -work work +incdir+C:/TEC/Segundo\ Semestre\ 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus {C:/TEC/Segundo Semestre 2022/taller/Proyecto/desencriptador-de-imagenes/ProyectoQuartus/memoryGetterTest.sv}
 
