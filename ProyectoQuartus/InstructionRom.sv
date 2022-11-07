e5c050ced53005de59cb3d68f04b54fbbf42f773
Luis Andrey Zuniga <luisandreyzuniga@gmail.com>
2022-11-07 17:29:07 -0600

    Changing memory output length
    Now returns 32 bits(full instruction)

--- ProyectoQuartus/InstructionRom.sv
+++ ProyectoQuartus/InstructionRom.sv
@@ -1,10 +1,10 @@
-module InstructionRom(input [7:0] address,output [7:0] Q);
+module InstructionRom(input [7:0] address,output [31:0] Q);
 	logic [7:0] memory [255:0];
 	initial $readmemh("Processor.mem",memory);
 	
 	
 
 	
-	assign Q=memory[address];
+	assign Q={memory[address],memory[address+1],memory[address+2],memory[address+3]};
 
 endmodule 
